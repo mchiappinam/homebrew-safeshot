d8f1e003331d95293cfb1cc5f98816de6f290f50
mchiappinam <mchiappinam@users.noreply.github.com>
2026-04-15 19:09:33 +0000

    v1.3.1

--- Casks/safeshot.rb
+++ Casks/safeshot.rb
@@ -1,14 +1,14 @@
 cask "safeshot" do
-  version "1.3.0"
+  version "1.3.1"
 
   on_intel do
     url "https://github.com/mchiappinam/SafeShot/releases/download/v#{version}/SafeShot-#{version}-macOS-Intel-x64.dmg"
-    sha256 "a11437d4e6e4973dd40c6f2975ddf050d11f5425dec98cd27bfc7c56cd853d30"
+    sha256 "0ccd39fdcf4b8af396c8b007d87758c0c5244e6d693359cce4afd9d350288d6f"
   end
 
   on_arm do
     url "https://github.com/mchiappinam/SafeShot/releases/download/v#{version}/SafeShot-#{version}-macOS-AppleSilicon-aarch64.dmg"
-    sha256 "eec42c4542049e05671b444f901076a468023e93b084517a39859b133ef10b58"
+    sha256 "2596688cedfc65b4d2dc499e81bfc6fabb03b9a122eef9cf4835a8592c2fac81"
   end
 
   name "SafeShot"
